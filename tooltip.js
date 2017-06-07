5d7d40610d11c16eccf358cea3b2d434209cd8e0
Jatheepan Kanthavel <theepan@cutedrops.com>
2017-06-07 05:27:50 -0400

    Added styles

--- tooltip.js
+++ tooltip.js
@@ -4,6 +4,7 @@ var Tooltip = {
 
         tooltip.innerText = text;
         element.parentNode.insertBefore(tooltip, element.nextSibling);
+        element.parentNode.classList.add('tooltip-parent');
     },
 
     hide: function() {
@@ -11,6 +12,7 @@ var Tooltip = {
 
         if(tooltip) {
             tooltip.parentNode.removeChild(tooltip);
+            tooltip.parentNode.classList.remove('tooltip-parent');
             this.tooltip = null;
         }
     },
@@ -25,4 +27,14 @@ var Tooltip = {
 
         return this.tooltip;
     }
+};
+
+var button = document.getElementById('button');
+var button2 = document.getElementById('button2');
+
+button.onclick = function() {
+    Tooltip.show(button, 'TEST');
+};
+button2.onclick = function() {
+    Tooltip.show(button2, 'TEST');
 };
