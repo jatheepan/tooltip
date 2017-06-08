c660614bfcf1d7bd146658e7b83278282f1052c0
Jatheepan Kanthavel <theepan@cutedrops.com>
2017-06-08 07:38:19 -0400

    Lot of bug fixes

--- angular/src/app/shared/tooltip/tooltip.ts
+++ angular/src/app/shared/tooltip/tooltip.ts
@@ -4,38 +4,36 @@ export let Tooltip = {
             // Hide any visible tooltips
             this.hide();
         }
+
         var tooltip = this.getTooltip();
-        var wrappedElement = this.getWrappedElement(element);
+
+        element.parentNode.insertBefore(tooltip, element.nextSibling);
 
         tooltip.innerText = text;
+        this.addTooltipStyle(element, tooltip);
 
-        wrappedElement.appendChild(tooltip);
-        wrappedElement.appendChild(element);
-
-        this.bodyClick = document.body.addEventListener('mouseup', function(e: any) {
+        this.bodyClick = document.addEventListener('mouseup', function(e: any) {
             var target = e.target;
             if(this.tooltipVisible && !target.classList.contains('tooltip')) {
-                this.hide();
+                this.hide(element);
             }
         }.bind(this));
 
         this.tooltipVisible = true;
     },
 
-    hide() {
-        var parent = this.wrappedElement.parentNode;
-
-        parent.replaceChild(this.orgElement, this.wrappedElement);
+    hide(element: any) {
+        element.parentNode.removeChild(this.getTooltip());
         this.tooltipVisible = false;
         this.wrappedElement = null;
         this.tooltip = null;
 
-        document.body.removeEventListener('mouseup', this.bodyClick);
+        document.removeEventListener('mouseup', this.bodyClick);
     },
 
     getTooltip() {
         if(!this.tooltip) {
-            var holder = document.createElement('div');
+            var holder = document.createElement('span');
 
             holder.classList.add('tooltip');
             holder.classList.add('top');
@@ -45,16 +43,12 @@ export let Tooltip = {
         return this.tooltip;
     },
 
-    getWrappedElement(element: any) {
-        if(!this.wrappedElement) {
-            this.orgElement = element;
-            var wrapper = document.createElement('div');
+    addTooltipStyle: function(element: any, tooltip: any) {
+        var elementPosition = element.getBoundingClientRect();
+        var elementWidth = element.offsetWidth;
 
-            wrapper.classList.add('tooltip-wrapper');
-            element.parentNode.replaceChild(wrapper, element);
-            this.wrappedElement = wrapper;
-        }
-
-        return this.wrappedElement;
+        tooltip.style.position = 'absolute';
+        tooltip.style.top = (elementPosition.top - (40)) + 'px';
+        tooltip.style.left = (elementPosition.left + (elementWidth / 2) - (tooltip.offsetWidth / 2)) + 'px';
     }
 };
