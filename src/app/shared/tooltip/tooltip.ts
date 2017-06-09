70475f149f96dd58774453fb0e740297d66949b0
Jatheepan Kanthavel <theepan@cutedrops.com>
2017-06-09 08:39:16 -0400

    bonus point 1

--- src/app/shared/tooltip/tooltip.ts
+++ src/app/shared/tooltip/tooltip.ts
@@ -1,11 +1,12 @@
 export let Tooltip = {
     show(element: any, text: string) {
+        this.element = element;
         if(this.tooltipVisible) {
             // Hide any visible tooltips
             this.hide();
         }
 
-        var tooltip = this.getTooltip();
+        let tooltip = this.getTooltip();
 
         element.parentNode.insertBefore(tooltip, element.nextSibling);
 
@@ -13,7 +14,7 @@ export let Tooltip = {
         this.addTooltipStyle(element, tooltip);
 
         this.bodyClick = document.addEventListener('mouseup', function(e: any) {
-            var target = e.target;
+            let target = e.target;
             if(this.tooltipVisible && !target.classList.contains('tooltip')) {
                 this.hide(element);
             }
@@ -25,7 +26,6 @@ export let Tooltip = {
     hide(element: any) {
         element.parentNode.removeChild(this.getTooltip());
         this.tooltipVisible = false;
-        this.wrappedElement = null;
         this.tooltip = null;
 
         document.removeEventListener('mouseup', this.bodyClick);
@@ -33,23 +33,33 @@ export let Tooltip = {
 
     getTooltip() {
         if(!this.tooltip) {
-            var holder = document.createElement('span');
+            let holder = document.createElement('span');
 
             holder.classList.add('tooltip');
-            holder.classList.add('top');
             this.tooltip = holder;
         }
 
         return this.tooltip;
     },
 
-    addTooltipStyle: function(element: any, tooltip: any) {
-        var tooltipWidth = tooltip.offsetWidth;
-        var elementPosition = element.getBoundingClientRect();
-        var elementWidth = element.offsetWidth;
+    addTooltipStyle(element: any, tooltip: any) {
+        let offset = 6;
+        let tooltipWidth = tooltip.offsetWidth;
+        let tooltipHeight = tooltip.offsetHeight;
+        let elementPosition = element.getBoundingClientRect();
+        let elementWidth = elementPosition.width;
+        let elementHeight = elementPosition.height;
 
         tooltip.style.position = 'absolute';
-        tooltip.style.top = (elementPosition.top - (40)) + 'px';
         tooltip.style.left = (elementPosition.left + (elementWidth / 2) - (tooltipWidth / 2)) + 'px';
+
+        if(tooltipHeight > elementPosition.top) {
+            tooltip.style.top = (elementPosition.top + (elementHeight + offset)) + 'px';
+            tooltip.classList.add('bottom');
+        } else {
+            tooltip.style.top = (elementPosition.top - (tooltipHeight + offset)) + 'px';
+            tooltip.classList.add('top');
+        }
+
     }
 };
